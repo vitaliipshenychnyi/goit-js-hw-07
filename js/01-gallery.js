4136a76f424eb10b59234d179749c78d1d11568b
VitaliiPshenychniy <4kvaap@gmail.com>
2023-03-23 13:35:57 +0200

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+// Створення і рендер розмітки на підставі масиву даних galleryItems
+const galleryListEl = document.querySelector('.gallery');
+galleryListEl.insertAdjacentHTML('beforeend', createGalleryItem(galleryItems));
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+      </a>
+      </li>`,
+    )
+    .join('');
+}
+
+// Отримання url великого зображення та відкриття модального вікна по кліку на елементі галереї
+galleryListEl.addEventListener('click', onClick);
+
+function onClick(event) {
+  event.preventDefault();
+
+  const modalWindowImg = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" width="800" height="600">
+  `);
+  modalWindowImg.show();
+
+  // Закриття модального вікна після натискання клавіші Escape
+  galleryListEl.addEventListener('keydown', closeOriginalImg);
+  function closeOriginalImg(event) {
+    if (event.code === 'Escape') {
+      modalWindowImg.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+// Створення і рендер розмітки на підставі масиву даних galleryItems
+const galleryListEl = document.querySelector('.gallery');
+galleryListEl.insertAdjacentHTML('beforeend', createGalleryItem(galleryItems));
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}">
+      </a>
+      </li>`,
+    )
+    .join('');
+}
+
+// Ініціалізація бібліотеки SimpleLightbox
+const slideOriginalImg = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
